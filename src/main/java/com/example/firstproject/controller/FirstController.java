b08da92582cbf6c04534747ff6fe19b2353de378
parkminsuyang <shyeon16@ewha.ac.kr>
2025-11-14 01:37:40 +0900

    Model 수정

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,12 +1,14 @@
 package com.example.firstproject.controller;
-
+import org.springframework.ui.Model; //Model 클래스 패키지 자동 임포트
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class FirstController {
     @GetMapping("/hi")
-    public String niceToMeetYou(){
+    public String niceToMeetYou(Model model){
+        model.addAttribute("username", "홍팍");
         return "greetings"; //greetings.mustache 파일 반환
     }
 }
