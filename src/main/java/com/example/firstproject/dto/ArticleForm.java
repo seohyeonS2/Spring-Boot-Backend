eec4baddeb604fd0e2be14822a4ce93042dd2c13
parkminsuyang <shyeon16@ewha.ac.kr>
2025-11-22 21:00:37 +0900

    @AllArgsConstructor, @ToString 어노테이션 추가

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,25 +1,17 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
     private String title; //제목을 받을 필드
     private String content; //내용을 받을 필드
 
-    public ArticleForm(String title, String content) {
-        // 전송받은 제목과 내용을 필드에 저장
-        this.title = title;
-        this.content = content;
-    }
-
     // 데이터를 잘 받았는지 확인
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
+
 
     public Article toEntity() {
         return new Article(null,title,content);
