8e6528c1cf040702af28615e2901305b97cdb790
parkminsuyang <shyeon16@ewha.ac.kr>
2025-11-28 03:13:38 +0900

    delete() 메서드 추가

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -87,4 +87,17 @@ public class ArticleController {
         //3. 수정 결과 페이지로 리다이렉트하기
         return "redirect:/articles/"+articleEntity.getId();
     }
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id){
+        log.info("삭제 요청이 들어왔습니다!!");
+        //1. 삭제할 대상 가져오기
+        Article target=articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+        //2. 대상 엔티티 삭제하기
+        if(target!=null){
+            articleRepository.delete(target);
+        }
+        //3. 결과 페이지로 리다이렉트하기
+        return "redirect:/articles";
+    }
 }
