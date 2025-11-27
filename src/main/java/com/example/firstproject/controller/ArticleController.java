560faa231f3398028523e479fd622d83e613131d
parkminsuyang <shyeon16@ewha.ac.kr>
2025-11-28 03:25:51 +0900

    삭제 메세지 출력

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -88,7 +89,7 @@ public class ArticleController {
         return "redirect:/articles/"+articleEntity.getId();
     }
     @GetMapping("/articles/{id}/delete")
-    public String delete(@PathVariable Long id){
+    public String delete(@PathVariable Long id, RedirectAttributes rttr){
         log.info("삭제 요청이 들어왔습니다!!");
         //1. 삭제할 대상 가져오기
         Article target=articleRepository.findById(id).orElse(null);
@@ -96,6 +97,7 @@ public class ArticleController {
         //2. 대상 엔티티 삭제하기
         if(target!=null){
             articleRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제됐습니다!");
         }
         //3. 결과 페이지로 리다이렉트하기
         return "redirect:/articles";
