676cdb9f185c8aa129709943302e16e60cd67c76
parkminsuyang <shyeon16@ewha.ac.kr>
2025-11-27 15:54:51 +0900

    리다이렉트 수정(상세페이지 이동)

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -38,7 +38,7 @@ public class ArticleController {
         Article saved=articleRepository.save(article);
         log.info(saved.toString());
         //System.out.println(saved.toString());
-        return "";
+        return "redirect:/articles/"+saved.getId();
     }
     @GetMapping("/articles/{id}") //데이터 조회 요청 접수
     public String show(@PathVariable Long id, Model model){//매개변수로 id 받아 오기
