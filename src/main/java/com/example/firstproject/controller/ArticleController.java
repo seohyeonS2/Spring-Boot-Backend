4ad335c99ae3da19ba8dae6d036e4d8819d3e998
parkminsuyang <shyeon16@ewha.ac.kr>
2025-11-28 01:51:02 +0900

    edit() 메서드 추가

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -59,4 +59,14 @@ public class ArticleController {
         // 3. 뷰 페이지 설정하기
         return "articles/index";
     }
+
+    @GetMapping("/articles/{id}/edit") //URL 요청 접수
+    public String edit(@PathVariable Long id, Model model){
+        //수정할 데이터 가져오기
+        Article articleEntitiy=articleRepository.findById(id).orElse(null);
+        //모델에 데이터 등록하기
+        model.addAttribute("article", articleEntitiy);
+        //뷰 페이지 설정하기
+        return "articles/edit";
+    }
 }
