59c88a22f4a2f3357fe6eb0f32d2c78668a41958
parkminsuyang <shyeon16@ewha.ac.kr>
2025-12-27 00:44:27 +0900

    13.2.2 index() 테스트하기

--- src/test/java/com/example/firstproject/service/ArticleServiceTest.java
+++ src/test/java/com/example/firstproject/service/ArticleServiceTest.java
@@ -1,8 +1,13 @@
 package com.example.firstproject.service;
 
+import com.example.firstproject.entity.Article;
 import org.junit.jupiter.api.Test; //Test 패키지 임포트
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*; //앞으로 사용할 수 있는 패키지 임포트
 
@@ -12,5 +17,14 @@ class ArticleServiceTest {
     ArticleService articleService; //articleService 객체 주입
     @Test //해당 메서드가 테스트 코드임을 선언
     void index() {
+        //1. 예상 데이터
+        Article a=new Article(1L, "가가가가", "1111");
+        Article b=new Article(2L, "나나나나", "2222");
+        Article c=new Article(3L, "다다다다", "3333");
+        List<Article> expected=new ArrayList<Article>(Arrays.asList(a,b,c));
+        //2. 실제 데이터
+        List<Article> articles=articleService.index();
+        //3. 비교 및 검증
+        assertEquals(expected.toString(), articles.toString());
     }
 }
