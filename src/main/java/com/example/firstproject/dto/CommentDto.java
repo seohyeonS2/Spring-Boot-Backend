f8f141a2f80c24f0074553ba84bcac0b2120aa51
parkminsuyang <shyeon16@ewha.ac.kr>
2026-01-03 20:12:53 +0900

    15.3.2 요청을 처리할 서비스 만들기

--- src/main/java/com/example/firstproject/dto/CommentDto.java
+++ src/main/java/com/example/firstproject/dto/CommentDto.java
@@ -1,5 +1,6 @@
 package com.example.firstproject.dto;
 
+import com.example.firstproject.entity.Comment;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,4 +15,13 @@ public class CommentDto {
     private Long articleId;
     private String nickname;
     private String body;
+
+    public static CommentDto createCommentDto(Comment comment) { //생성 메서드(객체 생성 없이 호출 가능)
+        return new CommentDto(
+                comment.getId(), //댓글 엔티티의 id
+                comment.getArticle().getId(), //댓글 엔티티가 속한 부모 게시글의 id
+                comment.getNickname(), //댓글 엔티티의 nickname
+                comment.getBody() //댓글 엔티티의 body
+                );
+    }
 }
--- src/main/java/com/example/firstproject/service/CommentService.java
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -1,10 +1,15 @@
 package com.example.firstproject.service;
 
+import com.example.firstproject.dto.CommentDto;
 import com.example.firstproject.entity.Article;
+import com.example.firstproject.entity.Comment;
 import com.example.firstproject.repository.ArticleRepository;
 import com.example.firstproject.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service //서비스로 선언
 public class CommentService {
@@ -12,4 +17,19 @@ public class CommentService {
     private CommentRepository commentRepository;
     @Autowired
     private ArticleRepository articleRepository;
+
+    public List<CommentDto> comments(Long articleId) {
+        //1. 댓글 조회
+        List<Comment> comments=commentRepository.findByArticleId(articleId);
+        //2. 엔티티 -> DTO변환
+        List<CommentDto> dtos=new ArrayList<CommentDto>();
+        for(int i=0; i<comments.size(); i++){ //조회한 댓글 엔티티 수만큼 반복하기
+            Comment c=comments.get(i);//조회한 댓글 엔티티 하나씩 가져오기
+            CommentDto dto=CommentDto.createCommentDto(c); //엔티티를 DTO로 변환
+            dtos.add(dto); //변환한 DTO를 dtos리스트에 삽입
+        }
+        //3. 결과 반환
+        return dtos;
+
+    }
 }
